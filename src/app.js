1555b3d8aa522e7a35975e942d1be2a541452acd
Nathan <160797692+NathanMB77@users.noreply.github.com>
2024-10-04 22:38:49 +0000

    doners

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domain = [".com", ".net", ".us", ".io"];
+  let domainName = "";
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < domain.length; l++) {
+          console.log(pronoun[i] + adj[j] + noun[k] + domain[l]);
+          domainName += pronoun[i] + adj[j] + noun[k] + domain[l] + "<br>";
+        }
+      }
+    }
+  }
+
+  document.querySelector("#domain").innerHTML = domainName;
 };
